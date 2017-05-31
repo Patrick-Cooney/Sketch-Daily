1825a28da0a94fdaf441289c1124f7830a89d7db
Patrick Cooney <patrick@robotsintre.es>
2017-05-30 21:35:32 -0500

    Basic Working App
    fleshed out UI that updates correctly based on reddit API

--- Sketch Daily/ViewController.swift
+++ Sketch Daily/ViewController.swift
@@ -7,12 +7,41 @@
 //
 
 import UIKit
+import Alamofire
+import AlamofireSwiftyJSON
+import SwiftyJSON
+
+var sketch: String = ""
+var date: String = ""
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    @IBOutlet weak var sketchLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let parameters: Parameters = ["sort": "new"]
+        Alamofire.request("https://www.reddit.com/r/sketchdaily/new.json", method: .get, parameters: parameters, encoding: URLEncoding.default).responseSwiftyJSON { response in
+            switch response.result {
+            case .success(let json):
+                // Use SwiftyJSON instance
+                let topicAndDate = json["data"]["children"][0]["data"]["title"].string
+                let topicComponents = topicAndDate?.components(separatedBy: "-")
+                date = topicComponents![0]
+                sketch = topicComponents![1]
+                self.sketchLabel.text = sketch.capitalized
+                self.dateLabel.text = date
+                
+            case .failure(let error):
+                // Handle error
+                print("Error: \(error)")
+            }
+          
+        }
+
     }
 
     override func didReceiveMemoryWarning() {
